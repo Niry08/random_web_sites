30a3bb1b21ca10626fa7c84b6a42a9937f008883
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-03-06 09:22:28 +0100

    Add games and update main page layout

---
+++ list_web_sites.js
@@ -0,0 +1,9 @@
+const sites = [
+    'scroll_game/scroll_game.html',
+    'reflex_game/reflex_game.html',
+];
+
+function getRandomSite() {
+    const randomIndex = Math.floor(Math.random() * sites.length);
+    window.location.href = sites[randomIndex];
+}
